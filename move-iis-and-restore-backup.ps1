bb0999522f6104107fe4581d446ad5e25db1e9ef
Kadir Avcı <avcikadir@gmail.com>
2021-01-26 11:19:15 +0300

    Delete move-iis-and-restore-backup.ps1

--- move-iis-and-restore-backup.ps1
+++
@@ -1,9 +0,0 @@
-# Move IIS to D drive
-# If you have EBS Volume you can download that script from "https://gallery.technet.microsoft.com/scriptcenter/Move-IIS-to-another-drive-99f9f741"
-& "D:\PowerShell\move_iss.ps1" "D"
-
-# Copy backup to InetSrv
-Copy-Item "D:\IISBackups\BackupName" "C:\Windows\system32\inetsrv\backup\"
-
-# Restore backup
-C:\Windows\system32\inetsrv\appcmd.exe restore backup BackupName
